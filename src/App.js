765caa2c74aa7c74c7e99d5f958c1ac3a071cad0
Jack Zhou <jackmkj@gmail.com>
2022-01-26 00:09:28 +1100

    React deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as WorkIcon } from "./work.svg";
+import { ReactComponent as SchoolIcon } from "./school.svg";
+
+import timelineElements from "./timelineElements";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
 
 function App() {
+  let workIconStyles = { background: "#06D6A0" };
+  let schoolIconStyles = { background: "#f9c74f" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Jack Zhou</h1>
+      <p className="linkedin"><a href="https://linkedin.com/in/feng-jack-zhou-b46a69111">LinkedIn</a></p>
+      <h2 className="email">Email:jackmkj@gmail.com</h2>
+      <VerticalTimeline>
+        {timelineElements.map((element) => {
+          let isWorkIcon = element.icon === "work";
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== "";
+
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            >
+              
+              <div className="vertical-timeline-element-logo"> 
+                {element.logo}
+              </div>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.company}
+                {/* {element.logo} */}
+              </h5>
+              <h3 className="vertical-timeline-element-title">
+                {element.position} 
+              </h3>
+
+              <p id="description1">{element.description1}</p>
+              <p id="description2">{element.description2}</p>
+              {showButton && (
+                <a
+                  className={`button ${isWorkIcon ? "workButton" : "schoolButton"
+                    }`}
+                  href="/"
+                >
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          );
+        })}
+      </VerticalTimeline>
     </div>
   );
 }
---
+++ src/timelineElements.js
@@ -0,0 +1,70 @@
+let timelineElements = [
+  {
+    id: 7,
+    company: "Department of Treasury ",
+    position: "ICT Graduate",
+    description1: "- Data Analysis",
+    description2: "- Business Intelligence",
+    // buttonText: "View SSRS Projects",
+    date: "December 2021 - present",
+    icon: "work",
+    logo: <img src="https://www.tas.gov.au/stds/tasgov_large.gif" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  },
+  {
+    id: 1,
+    company: "Huon Aquaculture Hobart, Tasmania",
+    position: "Application and IT Support Officer",
+    description1: "- Business reporting, Data Analysis and Visualisation",
+    description2: "- Business applications configuration and maintenance",
+    // buttonText: "View SSRS Projects",
+    date: "Feburary 2020 - December 2021",
+    icon: "work",
+    logo: <img src="https://upload.wikimedia.org/wikipedia/en/e/e9/Huon_Aquaculture_logo.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  },
+  {
+    id: 2,
+    company: "Tassal Hobart, Tasmania ",
+    position: "Harvest Attendant",
+    description1: "- Operate Cflow live-fish handling and management systems on the harvest vessel.",
+    description2: "- Undertake daily Salmon harvest and offload tasks.",
+    // buttonText: "View Frontend Projects",
+    date: "November 2017 - Feburary 2020",
+    icon: "work",
+    logo: <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Tassal_logo.png/220px-Tassal_logo.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  },
+  {
+    id: 3,
+    company: "University of Tasmania Hoabart, Tasmania",
+    position: "Master of Information Technology and Systems",
+    description1: "Programming, Database Management",
+    description2: "Networking",
+    // buttonText: "View Frontend Projects",
+    date: "March 2017 - July 2019",
+    icon: "school",
+    logo: <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/60/UTasLogo.png/150px-UTasLogo.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  },
+  // {
+  //   id: 4,
+  //   company: "Mantra Hotel Canberra, ACT",
+  //   position: "Housekeeper",
+  //   description1: "- Work on a team of 7 staff members to service 30 apartment rooms daily",
+  //   description2: "- Performing a variety of cleaning activities",
+  //   // buttonText: "View Frontend Projects",
+  //   date: "March 2016 - June 2016",
+  //   icon: "work",
+  //   logo: <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LcxNzeLIc7snLuCL2tOu3zyZ6NaruKHaEZJ18_SwkggGITxtv5juj5-d_iDllyVZLw&usqp=CAU" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  // },
+  {
+    id: 5,
+    company: "Zhejiang Gongshang University, Hangzhou, China",
+    position: " Bachelor of Sales Management",
+    description1: "Marketing and Sales Management",
+    description2: "Business Analysis",
+    // buttonText: "View Frontend Projects",
+    date: "October 2011 - July 2015",
+    icon: "school",
+    logo: <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Snap2.PNG/240px-Snap2.PNG" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  },
+];
+
+export default timelineElements;
