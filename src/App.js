7defbf07afb12d8dfbe57f6a34db3e42d2c2a705
Jack <jackmkj@gmail.com>
2024-12-24 15:03:37 +1100

    Update background colour and new jobs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import { ReactComponent as WorkIcon } from "./work.svg";
 import { ReactComponent as SchoolIcon } from "./school.svg";
-
+// improt database
 import timelineElements from "./timelineElements";
-
+//import pluginnpm
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -15,11 +15,16 @@ function App() {
   let workIconStyles = { background: "#06D6A0" };
   let schoolIconStyles = { background: "#f9c74f" };
 
+  const handleEmailClick = () => {
+    window.location.href = "mailto:jackmkj@gmail.com";
+  };
+
   return (
     <div>
-      <h1 className="title">Jack Zhou</h1>
+      <h1 className="title">Hi, I'm Jack Zhou</h1>
       <p className="linkedin"><a href="https://linkedin.com/in/jack-zhou-b46a69111">LinkedIn</a></p>
-      <h2 className="email">Email:jackmkj@gmail.com</h2>
+      <h2 className="email" onClick={handleEmailClick}>Email: jackmkj@gmail.com</h2>
+      <h2 className="linkedin">https://www.linkedin.com/in/jzhou-da/</h2>
       <VerticalTimeline>
         {timelineElements.map((element) => {
           let isWorkIcon = element.icon === "work";
@@ -37,19 +42,24 @@ function App() {
               icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
             >
               
-              <div className="vertical-timeline-element-logo"> 
+              {/* <div className="vertical-timeline-element-logo"> 
                 {element.logo}
-              </div>
-              <h5 className="vertical-timeline-element-subtitle">
-                {element.company}
-                {/* {element.logo} */}
-              </h5>
+              </div> */}
               <h3 className="vertical-timeline-element-title">
                 {element.position} 
               </h3>
-
-              <p id="description1">{element.description1}</p>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.company}
+              </h5>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.location}
+              </h5>
+             {/*  <p>{element.description}</p> */}
+              <p className="description" dangerouslySetInnerHTML={{ __html: element.description }}></p>
+              {/* <p id="description1">{element.description1}</p>
               <p id="description2">{element.description2}</p>
+              <p id="description3">{element.description3}</p>
+              <p id="description4">{element.description4}</p> */}
               {showButton && (
                 <a
                   className={`button ${isWorkIcon ? "workButton" : "schoolButton"
@@ -63,7 +73,7 @@ function App() {
           );
         })}
       </VerticalTimeline>
-      <h6 className="footer">"To be continued~~~~~"</h6>
+      <h6 className="footer">"WOOHOO"</h6>
     </div>
   );
 }
--- src/timelineElements.js
+++ src/timelineElements.js
@@ -1,67 +1,84 @@
 let timelineElements = [
   {
-    id: 7,
-    company: "Department of Treasury and Finance",
+    //id: 7,
     position: "Data Analyst",
-    description1: "- Undertake research and analysis of transactions, taxpayer behaviours and trends to identify issues and recommend compliance activity",
-    description2: "- Use analytical methodologies and software tools to manipulate data and carry out research, analysis and presentation of information to internal stakeholders",
+    company: "Commonwealth Bank of Australia",
+    location: "Sydney, Australia",
+    description: `
+      • Worked on end-to-end customer journey campaigns to derive actionable and impactful customer insights that will help drive the strategic goals of the business.<br />
+      • Created data-driven segmentation, eligibility and contact channel selection to ensure every customer touchpoint is highly relevant and compelling.<br />
+      • Managed campaign delivery in collaboration with marketing and product squads.<br />
+      • Collected, monitored and analysed data from multiple sources to identify trends and patterns to inform business strategy and drive growth.<br />
+      • Developed, maintained, and educated stakeholder relationships with a diverse group of technical and non-technical leadership teams.
+    `,
     // buttonText: "View SSRS Projects",
-    date: "December 2021 - present",
+    date: "Apr 2024 - Present",
     icon: "work",
     logo: <img src="https://www.communications.tas.gov.au/__data/assets/image/0021/17355/100079-Tas-Gov_no-tag_rgb_vert.jpg" alt="alternatetext" style={{ textAlign: 'right' }}></img>
   },
   {
-    id: 1,
-    company: "Huon Aquaculture Hobart, Tasmania",
-    position: "Data Analyst / Report Developer",
-    description1: "- Data Analysis with SQL queries and stored procedures",
-    description2: "- Design and deploy analytical reports (SSRS,SSIS,Power BI)",
+    //id: 6,
+    company: "Prezzee",
+    position: "Data Analyst",
+    location: "Melbourne, Australia",
+    description: `
+      • Developed and maintained dbt data models and optimised ELT processes, resulting in a 50% faster processing time.<br />
+      • Led monthly analytics presentations to senior stakeholders, facilitating better decision-making for BNPL customers.<br />
+      • Conducted financial impact analyses for Strategic Partners, identifying user behaviours and potential loss.<br />
+      • Provided actionable reporting and analysis across various business units, improving marketing ROI and partner relationships.<br />
+      • Spearheaded new feature data analysis projects, contributing to an 80% increase in new feature adoption and a 10% improvement in customer signup conversion rates.<br />
+      • Led Looker cost analysis to reduce monthly user license costs.<br />
+      • Enhanced the performance of the ChatGPT-linked chatbot, reducing API costs per session.
+    `,
     // buttonText: "View SSRS Projects",
-    date: "Feburary 2020 - December 2021",
+    date: "Sep 2022 - Apr 2024",
     icon: "work",
-    logo: <img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Huon_Aqua_Logo_Web.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+    logo: <img src="https://www.communications.tas.gov.au/__data/assets/image/0021/17355/100079-Tas-Gov_no-tag_rgb_vert.jpg" alt="alternatetext" style={{ textAlign: 'right' }}></img>
   },
   {
-    id: 2,
-    company: "Tassal Hobart, Tasmania ",
-    position: "Harvest Attendant",
-    description1: "- Undertake daily salmon harvest and offload tasks",
-    description2: "- Communicate with site-management and onshore offload crew to ensure that works are carried out in synergy with other operations",
-    // buttonText: "View Frontend Projects",
-    date: "November 2017 - Feburary 2020",
+    id: 4,
+    company: "Department of Treasury and Finance",
+    position: "Data Analyst",
+    location: "Hobart, Australia",
+    description1: "- Undertake research and analysis of transactions, taxpayer behaviours and trends to identify issues and recommend compliance activity",
+    description2: "- Use analytical methodologies and software tools to manipulate data and carry out research, analysis and presentation of information to internal stakeholders",
+    // buttonText: "View SSRS Projects",
+    date: "Dec 2021 - Sep 2022",
     icon: "work",
-    logo: <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Tassal_logo.png/220px-Tassal_logo.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+    logo: <img src="https://www.communications.tas.gov.au/__data/assets/image/0021/17355/100079-Tas-Gov_no-tag_rgb_vert.jpg" alt="alternatetext" style={{ textAlign: 'right' }}></img>
   },
   {
     id: 3,
+    company: "Huon Aquaculture Hobart, Tasmania",
+    position: "Data Analyst / Report Developer",
+    location: "Hobart, Australia",
+    description1: "- Data Analysis with SQL queries and stored procedures",
+    description2: "- Design and deploy analytical reports (SSRS,SSIS,Power BI)",
+    // buttonText: "View SSRS Projects",
+    date: "Feb 2020 - Dec 2021",
+    icon: "work",
+    //logo: <img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Huon_Aqua_Logo_Web.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
+  },
+  {
+    id: 2,
     company: "University of Tasmania Hoabart, Tasmania",
     position: "Master of Information Technology and Systems",
     //description1: "Programming, Database Management",
    // description2: "Networking",
     // buttonText: "View Frontend Projects",
-    date: "March 2017 - July 2019",
+    date: "Feb 2020 - Dec 2021",
     icon: "school",
     logo: <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/60/UTasLogo.png/150px-UTasLogo.png" alt="alternatetext" style={{ textAlign: 'right' }}></img>
   },
-  // {
-  //   id: 4,
-  //   company: "Mantra Hotel Canberra, ACT",
-  //   position: "Housekeeper",
-  //   description1: "- Work on a team of 7 staff members to service 30 apartment rooms daily",
-  //   description2: "- Performing a variety of cleaning activities",
-  //   // buttonText: "View Frontend Projects",
-  //   date: "March 2016 - June 2016",
-  //   icon: "work",
-  //   logo: <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LcxNzeLIc7snLuCL2tOu3zyZ6NaruKHaEZJ18_SwkggGITxtv5juj5-d_iDllyVZLw&usqp=CAU" alt="alternatetext" style={{ textAlign: 'right' }}></img>
-  // },
+
   {
-    id: 5,
+    id: 1,
     company: "Zhejiang Gongshang University, Hangzhou, China",
     position: " Bachelor of Sales Management",
    // description1: "Marketing and Sales Management",
    // description2: "Business Analysis",
     // buttonText: "View Frontend Projects",
-    date: "October 2011 - July 2015",
+    date: "Oct 2011 - Jul 2015",
     icon: "school",
     logo: <img src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d6/Snap2.PNG/240px-Snap2.PNG" alt="alternatetext" style={{ textAlign: 'right' }}></img>
   },
